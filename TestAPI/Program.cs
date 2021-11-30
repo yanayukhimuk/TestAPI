e4cbed5dc8b0b5151737c9dc3c42a4b5f6e2659d
Yana Yukhimuk <61915492+yanayukhimuk@users.noreply.github.com>
2021-11-30 12:26:57 +0300

    Add project files.

---
+++ TestAPI/Program.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Xceed.Wpf.Toolkit;
+
+namespace TestAPI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string response = MakeRequest();
+            File.WriteAllText(@"C:\TestApi\json.txt", response);
+            //Root usersSearch = JsonConvert.DeserializeObject<Root>(response);
+            //MessageBox.Show(usersSearch.name[0].ToString(), "result");
+        }
+        public static string MakeRequest()
+        {
+            string responseBody = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://jsonplaceholder.typicode.com/users"); //какие параметры
+            request.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            using (Stream s = response.GetResponseStream())
+            {
+                using (StreamReader r = new StreamReader(s))
+                {
+                    responseBody = r.ReadToEnd();
+                }
+            }
+            return responseBody;
+        }
+        public static string MakeRequestPost()
+        {
+            string responseBody = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://jsonplaceholder.typicode.com/users"); //какие параметры
+            request.Method = "POST";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            using (Stream st = request.GetRequestStream())
+            {
+                using (StreamWriter wr = new StreamWriter(st))
+                {
+                    wr.Write("Hello!");
+                }
+            }
+            return responseBody;
+        }
+
+        public static string MakeRequestPost2()
+        {
+            string responseBody = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://jsonplaceholder.typicode.com/users"); //какие параметры
+            request.Method = "POST";
+
+            byte[] byteArr = Encoding.ASCII.GetBytes("");
+            request.GetRequestStream().Write(byteArr, 0, byteArr.Length);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            using (Stream s = response.GetResponseStream())
+            {
+                using (StreamReader r = new StreamReader(s))
+                {
+                    responseBody = r.ReadToEnd();
+                }
+            }
+            return responseBody;
+        }
+    }
+}
